c86d0ce5ed8b424d35025177bb33717f7ad62cab
PauSansa <pausansa59@gmail.com>
2023-09-22 11:06:14 +0200

    first commit

---
+++ src/main/java/com/sansa/entretainmentapi/EntretainmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.sansa.entretainmentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntretainmentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntretainmentApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sansa/entretainmentapi/EntretainmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansa.entretainmentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntretainmentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
